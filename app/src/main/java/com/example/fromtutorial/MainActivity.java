b7cbfd1323b3392ab71a37db25ba8e723c9ea553
tareqrosul <40066896+tareqrosul@users.noreply.github.com>
2021-08-16 22:25:26 +0600

    this is the end

--- app/src/main/java/com/example/fromtutorial/MainActivity.java
+++ app/src/main/java/com/example/fromtutorial/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.fromtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -12,12 +17,21 @@ import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch (view.getId())
         {
             case R.id.btn1:
                 Toast.makeText(this, "This is Using Switch case", Toast.LENGTH_SHORT).show();
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    //deprecated in API 26
+                    v.vibrate(500);
+                }
                 break;
             case R.id.next:
                 Intent intent = new Intent(MainActivity.this, next_activity.class);
@@ -42,6 +56,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonThird=findViewById(R.id.btn1);
         buttonThird.setOnClickListener(this);
 
+        buttonThird.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    //deprecated in API 26
+                    v.vibrate(500);
+                }
+                Toast.makeText(MainActivity.this, "This Button is long pressed ", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
         nextPage=findViewById(R.id.next);
         nextPage.setOnClickListener(this);
 
@@ -52,6 +82,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 txt=findViewById(R.id.firsText);
                 txt.setText("this is setOnClickListener");
                 Toast.makeText(MainActivity.this, "This is second", Toast.LENGTH_SHORT).show();
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    //deprecated in API 26
+                    v.vibrate(500);
+                }
             }
         });
     }
--- app/src/main/java/com/example/fromtutorial/next_activity.java
+++ app/src/main/java/com/example/fromtutorial/next_activity.java
@@ -2,11 +2,21 @@ package com.example.fromtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class next_activity extends AppCompatActivity {
-
+    EditText edtx;
+    TextView tv;
+    Button nextBTn1;
+    RelativeLayout RL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +24,17 @@ public class next_activity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_next_activity);
+        nextBTn1=findViewById(R.id.btnHello);
+        nextBTn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RL = (RelativeLayout) findViewById(R.id.RL);
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(RL.getWindowToken(), 0);
+                edtx=findViewById(R.id.editTxtName);
+                tv=findViewById(R.id.textView22);
+                tv.setText(edtx.getText().toString());
+            }
+        });
     }
 }
